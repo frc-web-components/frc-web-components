0f2ed8e669ba54dd508cf5a42f72b08f3f3d1495
Amory Galili <amorygalili@gmail.com>
2020-05-21 02:31:58 -0400

    Refacted model viewer. Added ability to configure which axis pairs with what rotation

--- src/components/model-viewer/index.js
+++ src/components/model-viewer/index.js
@@ -24,15 +24,17 @@ class ModelViewer extends Webbit {
   }
 
   static get properties() {
-    // X is Pitch, Y is Roll, Z is Yaw
     return {
       src: { type: String },
       pitch: { type: Number },
       roll: { type: Number },
-      yaw: { type: Number },
+      yaw: { type: Number, primary: true },
       pitchOffset: { type: Number },
       rollOffset: { type: Number },
       yawOffset: { type: Number },
+      pitchAxis: { type: String, get() { return this._pitchAxis.toLowerCase(); } },
+      rollAxis: { type: String, get() { return this._rollAxis.toLowerCase(); } },
+      yawAxis: { type: String, get() { return this._yawAxis.toLowerCase(); } },
     };
   }
 
@@ -44,14 +46,30 @@ class ModelViewer extends Webbit {
     this.pitchOffset = 0;
     this.rollOffset = 0;
     this.yawOffset = 0;
+    this.pitchAxis = 'x';
+    this.rollAxis = 'y';
+    this.yawAxis = 'z';
     this.model = null;
     this.shouldAnimate = false;
   }
 
+  getQuaternion(roll, pitch, yaw) {
+    const { pitchAxis, yawAxis, rollAxis } = this;
+    const order = `${rollAxis}${pitchAxis}${yawAxis}`.toUpperCase();
+    const { x = 0, y = 0, z = 0 } = {
+      [pitchAxis]: pitch * rad,
+      [yawAxis]: yaw * rad,
+      [rollAxis]: roll * rad
+    };
+    return Quaternion.fromEuler(z, x, y,  order);
+  }
+
   transform() {
-    const q = Quaternion.fromEuler(this.yaw * rad, this.pitch * rad, this.roll * rad, 'YXZ');
-    const offsetQ = Quaternion.fromEuler(this.yawOffset * rad, this.pitchOffset * rad, this.rollOffset * rad, 'YXZ');
-    this.model.style.transform = `matrix3d(${q.conjugate().toMatrix4()}) matrix3d(${offsetQ.conjugate().toMatrix4()})`;
+    const q = this.getQuaternion(this.roll, this.pitch, this.yaw);
+    const offsetQ = this.getQuaternion(this.rollOffset, this.pitchOffset, this.yawOffset);
+    if (q !== null && offsetQ !== null) {
+      this.model.style.transform = `matrix3d(${q.conjugate().toMatrix4()}) matrix3d(${offsetQ.conjugate().toMatrix4()})`;
+    }
   }
 
   firstUpdated() {
