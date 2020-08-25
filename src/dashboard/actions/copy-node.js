af1dbad505a60f5483e7e2e45c0a7bd39d0f8474
Amory Galili <amorygalili@gmail.com>
2020-08-25 14:11:41 -0400

    Implemented copy node action

--- src/dashboard/actions/copy-node.js
+++ src/dashboard/actions/copy-node.js
@@ -1,4 +1,5 @@
 import Action from '../action';
+import { addElement, createElement } from './utils';
 
 export default class CopyNode extends Action {
 
@@ -7,9 +8,50 @@ export default class CopyNode extends Action {
       needsSelection: true,
       needsTarget: true
     });
+
+    this.previewedNode = null;
   }
 
-  execute({ wom, selectedNode }) {
-    alert(`Copying nodes hasn't been implemented yet!`);
+  removePreviewedNode(wom) {
+    if (this.previewedNode) {
+      this.previewedNode.remove();
+      wom.dispatchEvent('womPreviewNodeRemove', { node: this.previewNode });
+    }
+  }
+
+  deselect({ wom }) {
+    this.removePreviewedNode(wom);
+  }
+
+  contextChange({ wom, selectedNode, context }) {
+    this.removePreviewedNode(wom);
+    const { placement, targetedNode, slot } = context;
+
+    if (targetedNode) {
+      this.createPreviewedNode(selectedNode.getNode(), slot);
+      addElement(wom, this.previewedNode, targetedNode.getNode(), placement);
+      wom.dispatchEvent('womPreviewNodeAdd', { node: this.previewedNode });
+    }
+  }
+
+  createPreviewedNode(node, slot) {
+    this.previewedNode = node.cloneNode(true);
+    this.previewedNode.setAttribute('is-preview', '');
+    this.previewedNode.setAttribute('webbit-id', 'preview');
+    this.previewedNode.setAttribute('slot', slot === 'default' ? '' : slot);
+  }
+
+  execute({ wom, selectedNode, targetedNode, context }) {
+    this.removePreviewedNode(wom);
+    const { placement, slot } = context;
+
+    wom.addListenerOnce('womChange', () => {
+      wom.history.push(wom.getJson());
+    });
+
+    this.previewedNode.removeAttribute('is-preview');
+    this.previewedNode.setAttribute('webbit-id', selectedNode.getWebbitId());
+    addElement(wom, this.previewedNode, targetedNode.getNode(), placement);
+    this.createPreviewedNode(this.previewedNode, slot);
   }
 }
