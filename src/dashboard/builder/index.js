fb36122ba1d79d83f497978829acb9bfe308bab9
Amory Galili <amorygalili@gmail.com>
2020-08-26 04:11:55 -0400

    Began working on drag and drop

--- src/dashboard/builder/index.js
+++ src/dashboard/builder/index.js
@@ -11,6 +11,23 @@ class DashboardBuilder extends LitElement {
         width: 70%;
       }
 
+      [part=edit-container] {
+        position: relative;
+      }
+
+      [part=original-wom] {
+        opacity: 0;
+      }
+
+      [part=copy-wom] {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        pointer-events: none;
+      }
+
     `;
   }
 
@@ -42,6 +59,43 @@ class DashboardBuilder extends LitElement {
     }
   }
 
+  firstUpdated() {
+
+    this.wom.addListener('womActionSelect', () => {
+      this.requestUpdate();
+    });
+
+    this.wom.addListener('womActionDeselect', () => {
+      this.requestUpdate();
+    });
+
+    const observer = new MutationObserver((mutations) => {
+      for (let mutation of mutations) {
+        const { addedNodes } = mutation;
+        if (addedNodes.length > 0) {
+          const editContainer = [...addedNodes].find(element => {
+            return (
+              element.nodeName === 'DIV'
+              && element.getAttribute('part') === 'edit-container'
+            );
+          });
+          if (editContainer) {
+            const copyWom = editContainer.querySelector('[part=copy-wom]');
+            this.wom.getRootNode().getChildren().forEach(womNode => {
+              let clonedNode = womNode.getNode().cloneNode(true);
+              copyWom.appendChild(clonedNode);
+            });
+          }
+
+        }
+      }
+    });
+    observer.observe(this.shadowRoot, {
+      childList: true,
+      subtree: true,
+    });
+  }
+
   render() {
     return html`
       <wom-preview-box
@@ -52,7 +106,18 @@ class DashboardBuilder extends LitElement {
         .wom="${this.wom}"
       ></wom-new-element-preview-box>
       <div part="container">
-        <slot></slot>
+        ${this.wom.getSelectedActionId() === 'addNode' ? html`
+          <div part="edit-container">
+            <div part="copy-wom">
+            </div>
+            <div part="original-wom">
+              <slot></slot>
+            </div>
+          </div>
+        ` : html`
+          <slot></slot>
+        `}
+        
       </div>
     `;
   }
