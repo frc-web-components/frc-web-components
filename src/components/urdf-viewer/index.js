c37c32602cda59d6af002f153cb1c527f16caa1b
Amory Galili <amorygalili@gmail.com>
2020-09-07 15:31:22 -0400

    wip

--- src/components/urdf-viewer/index.js
+++ src/components/urdf-viewer/index.js
@@ -5,8 +5,19 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader';
 
+import { 
+  subscribe, 
+  subscribeAll,
+  getRawSource,
+  getRawSources, 
+  hasSourceProvider, 
+  sourceProviderAdded 
+} from '@webbitjs/store';
+
 
 customElements.define('urdf-viewer', URDFManipulator);
+
+const DEG2RAD = Math.PI / 180;
 
 const loadMeshFunc = (path, manager, done) => {
 
@@ -93,6 +104,30 @@ class UrdfViewer extends Webbit {
   constructor() {
     super();
     this.urdf = '';
+    this.unsubscribe = () => {};
+  }
+
+
+  setSources() {
+    const viewer = this.shadowRoot.querySelector('urdf-viewer');
+    this.unsubscribe();
+
+    if (this.sourceKey) {
+      this.unsubscribe = subscribe(this.sourceProvider, this.sourceKey, source => {
+        Object.getOwnPropertyNames(source).forEach(name => {
+          viewer.setAngle(name, source[name] * DEG2RAD);
+        });
+      }, true);
+    }
+  }
+
+  updated(changedProperties) {
+
+    if (changedProperties.has('sourceKey') || changedProperties.has('sourceProvider')) {
+      if (this.sourceProvider) {
+        this.setSources();
+      }
+    }
   }
 
   render() {
