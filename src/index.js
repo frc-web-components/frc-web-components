7790d68bb3cb37bfce59e424227056aca6fbbd2d
Amory Galili <amorygalili@gmail.com>
2021-02-22 15:48:35 -0500

    Created global dashboard API

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ import './dashboard';
 
 import { getExtensions } from './db';
 
+window.dashboard = {
+  addComponent: window.webbitRegistry.define,
+  LitElement: window.webbit.LitElement,
+  Component: window.webbit.Webbit,
+  css: window.webbit.css,
+  html: window.webbit.html,
+  svg: window.webbit.svg,
+  SourceProvider: window.webbitStore.SourceProvider,
+  addSourceProvider: window.webbitStore.addSourceProvider,
+  addSourceProviderType: window.webbitStore.addSourceProviderType,
+};
+
 async function initExtensions() {
   const extensions = await getExtensions();
   extensions.forEach(({ name, version, code }) => {
