ce4e0718e1e04851f6dae186074c3397b617e39f
Amory Galili <amorygalili@gmail.com>
2021-02-23 17:23:29 -0500

    About dialog now shows dashboard version number

--- src/dashboard/tools/about-dialog.js
+++ src/dashboard/tools/about-dialog.js
@@ -24,13 +24,25 @@ class AboutDialog extends LitElement {
             text-align: center;
           }
 
-          .about-dialog-content p {
+          .about-dialog-content h1 {
             font-size: 20px;
             font-weight: bold;
+            margin-bottom: 0px;
+          }
+
+          .about-dialog-content p {
+            font-size: 16px;
+            margin-top: 5px;
+            margin-bottom: 10px;
+          }
+
+          .about-dialog-content p span {
+            color: purple;
           }
         </style>
         <div class="about-dialog-content">
-          <p>FWC Dashboard</p>
+          <h1>FWC Dashboard</h1>
+          <p>Version <span>3.0.0</span></p>
           <vaadin-button>Close</vaadin-button>
         </div>
       `;
