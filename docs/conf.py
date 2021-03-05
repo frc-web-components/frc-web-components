759aac0959ac98960d4907fecd3bb17119547170
Amory Galili <amorygalili@gmail.com>
2021-03-04 21:39:40 -0500

    wip

--- docs/conf.py
+++ docs/conf.py
@@ -13,11 +13,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-from recommonmark.parser import CommonMarkParser
-from sphinx_markdown_parser.parser import MarkdownParser
-
-
-
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -43,8 +38,8 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['m2r']
-
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,10 +47,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_parsers = {'.md': MarkdownParser}
-
-source_suffix = ['.rst', '.md']
-# sourcse_suffix = '.rst'
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
