f3cb67f57f34894412b4975925fc6cd883827376
Amory Galili <amorygalili@gmail.com>
2021-03-04 20:30:02 -0500

    wip

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst-parser']
+extensions = ['recommonmark']
 
 
 # Add any paths that contain templates here, relative to this directory.
