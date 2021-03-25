56cf7305425efdd26f8762da25dfe0a0472b99a3
Amory Galili <amorygalili@gmail.com>
2021-03-24 23:21:38 -0400

    Exposed the render lit html function to global dashboard object

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './frc-dashboard';
 import './frc-dashboard-builder';
 import './dashboard';
 import './components';
+import { render } from 'lit-html';
 
 window.dashboard = {
   addComponent: window.webbitRegistry.define,
@@ -13,6 +14,7 @@ window.dashboard = {
   css: window.webbit.css,
   html: window.webbit.html,
   svg: window.webbit.svg,
+  render,
   SourceProvider: window.webbitStore.SourceProvider,
   addSourceProvider: window.webbitStore.addSourceProvider,
   addSourceProviderType: window.webbitStore.addSourceProviderType,
