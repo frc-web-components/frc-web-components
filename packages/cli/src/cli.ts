22bc548ff2c81ffa0a4ef896897b4be9ae9780f5
Amory Galili <amorygalili@gmail.com>
2022-08-14 14:37:44 -0400

    cli can now serve files in the directly it was run in. Dashboard script is injected automatically

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,13 +1,34 @@
 import { createServer } from 'vite';
+import { resolve } from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+const entry = process.cwd();
 
 (async () => {
   const server = await createServer({
     // any valid user config options, plus `mode` and `configFile`
     configFile: false,
-    root: __dirname,
+    root: entry,
     server: {
-      port: 1337,
+      open: '/',
     },
+    publicDir: __dirname,
+    plugins: [
+      createHtmlPlugin({
+        inject: {
+          tags: [
+            {
+              injectTo: 'body',
+              tag: 'script',
+              attrs: {
+                type: 'module',
+                src: '/dashboard.js',
+              },
+            },
+          ],
+        },
+      }),
+    ],
   });
   await server.listen();
 
