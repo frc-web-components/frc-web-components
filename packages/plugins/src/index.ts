249ebde9c3715c19ed53f39bf0acd4b3a91ff196
Amory Galili <amorygalili@gmail.com>
2022-08-18 23:54:16 -0400

    Exported individual plugins

--- packages/plugins/src/index.ts
+++ packages/plugins/src/index.ts
@@ -12,3 +12,9 @@ export default function addPlugins(dashboard: FrcDashboard): void {
   addVaadinElementsPlugin(dashboard);
   addWiredElementsPlugin(dashboard);
 }
+
+export const addDashboardComponents = addDashboardComponentsPlugin;
+export const addFrc = addFrcPlugin;
+export const addFrcSim = addFrcSimPlugin;
+export const addVaadinElements = addVaadinElementsPlugin;
+export const addWiredElements = addWiredElementsPlugin;
