f09baa84822b6cd61215df990f3a578d7f39459a
Amory Galili <amorygalili@gmail.com>
2022-08-18 21:30:52 -0400

    In the createDashboard function you must add plugins separately and can optionally pass in a custom dashboard element

--- packages/cli/src/dashboard.ts
+++ packages/cli/src/dashboard.ts
@@ -1,4 +1,6 @@
 import createDashboard from '@frc-web-components/frc-web-components';
+import addPlugins from '@frc-web-components/plugins';
 
 const dashboard = createDashboard(document.body);
+addPlugins(dashboard);
 (window as any).dashboard = dashboard;
--- packages/frc-web-components/src/index.ts
+++ packages/frc-web-components/src/index.ts
@@ -2,17 +2,12 @@ import './icons';
 import './theme';
 import './vaadin';
 import './ace-editor';
-import { html, render } from 'lit';
 import { FrcDashboard } from '@frc-web-components/dashboard';
-import addPlugins from '@frc-web-components/plugins';
 
-export default function createDashboard(element: HTMLElement): FrcDashboard {
+export default function createDashboard(element: HTMLElement, dashboardElementName = 'dashboard-root'): FrcDashboard {
   const dashboard = new FrcDashboard();
-  addPlugins(dashboard);
-
-  render(
-    html`<dashboard-root .dashboard=${dashboard}></dashboard-root>`,
-    element,
-  );
+  const dashboardElement = document.createElement(dashboardElementName);
+  (dashboardElement as any).dashboard = dashboard;
+  element.appendChild(dashboardElement);
   return dashboard;
 }
