17f62e2b8333a5f25d27720af3a41b439cc2b35f
shueja-personal <j.a.shue@outlook.com>
2022-09-07 23:24:08 -0700

    Added fwc-number component

--- packages/plugins/src/source-elements/index.ts
+++ packages/plugins/src/source-elements/index.ts
@@ -1,8 +1,10 @@
 import { FrcDashboard } from '@frc-web-components/dashboard';
 import { fwcBooleanConfig } from './fwc-boolean';
+import { fwcNumberConfig } from './fwc-number';
 
 export default function addSourceElements(dashboard: FrcDashboard): void {
   dashboard.addElements({
     'fwc-boolean': fwcBooleanConfig,
+    'fwc-number': fwcNumberConfig,
   }, 'Sources');
 }
