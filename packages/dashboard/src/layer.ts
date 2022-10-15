082b3009801478e0e0b9161b2dc2163ccb8dc3af
Amory Galili <amorygalili@gmail.com>
2022-10-14 20:49:05 -0400

    wip

--- packages/dashboard/src/layer.ts
+++ packages/dashboard/src/layer.ts
@@ -1,16 +1,24 @@
-import Dashboard from './dashboard';
+/* eslint-disable class-methods-use-this */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import FrcDashboard from './frc-dashboard';
 
 export default abstract class Layer {
-  static getElementRect(layerElement: HTMLElement, element: HTMLElement): {
-    left: number,
-    top: number,
-    width: number,
-    height: number,
+  protected dashboard: FrcDashboard;
+  protected id: string;
+  protected element!: HTMLElement;
+
+  static getElementRect(
+    layerElement: HTMLElement,
+    element: HTMLElement
+  ): {
+    left: number;
+    top: number;
+    width: number;
+    height: number;
   } {
-    const { left: containerLeft, top: containerTop } = layerElement.getBoundingClientRect();
-    const {
-      left, top, width, height,
-    } = element.getBoundingClientRect();
+    const { left: containerLeft, top: containerTop } =
+      layerElement.getBoundingClientRect();
+    const { left, top, width, height } = element.getBoundingClientRect();
     return {
       left: left - containerLeft,
       top: top - containerTop,
@@ -19,6 +27,50 @@ export default abstract class Layer {
     };
   }
 
-  abstract mount(element: HTMLElement, dashboard: Dashboard): void;
-  abstract unmount(element: HTMLElement, dashboard: Dashboard): void;
+  constructor(id: string, dashboard: FrcDashboard) {
+    this.id = id;
+    this.dashboard = dashboard;
+    dashboard.addComponent({
+      type: 'layer',
+      id,
+      mount: ({ element }) => {
+        this.element = element;
+        this.mount();
+        return () => {
+          this.unmount();
+        };
+      },
+    });
+    const layerElement = dashboard.create('layer', id, {});
+    if (layerElement) {
+      layerElement.setAttribute('slot', 'layer');
+      layerElement.setAttribute('layer-id', id);
+      layerElement.style.display = 'none';
+      layerElement.style.width = '100%';
+      layerElement.style.height = '100%';
+      layerElement.style.position = 'absolute';
+      layerElement.style.top = '0';
+      layerElement.style.boxSizing = 'border-box';
+      layerElement.style.pointerEvents = 'none';
+    }
+  }
+
+  show(): void {
+    this.element.style.display = 'block';
+  }
+
+  hide(): void {
+    this.element.style.display = 'none';
+  }
+
+  addPointerEvents(): void {
+    this.element.style.pointerEvents = 'all';
+  }
+
+  removePointerEvents(): void {
+    this.element.style.pointerEvents = 'none';
+  }
+
+  mount(): void {}
+  unmount(): void {}
 }
