a3f4a3780c7ac6b031aafcaf793843841c8273b3
amorygalili <amorygalili@gmail.com>
2022-10-25 00:58:46 -0400

    Removed old NT3 code

--- packages/plugins/src/frc/index.ts
+++ packages/plugins/src/frc/index.ts
@@ -1,12 +1,10 @@
 import { FrcDashboard } from '@frc-web-components/dashboard';
 import { SourceProvider } from '@webbitjs/store';
-// import NetworkTablesProvider from './networktables-provider';
 import Nt4Provider from './nt4/nt4-provider';
 import frcElements from './elements';
 import addTutorials from './tutorials';
 
 export default function addPlugin(dashboard: FrcDashboard): void {
-  // dashboard.addSourceProvider('NetworkTables', new NetworkTablesProvider());
   dashboard.addSourceProvider('NetworkTables', new Nt4Provider());
   dashboard.addSourceProvider('Demo', new SourceProvider());
   dashboard.setDefaultSourceProvider('NetworkTables');
