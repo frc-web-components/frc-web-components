a97315b8779a53bf002e71f8d2896a6ffde11c8c
amorygalili <amorygalili@gmail.com>
2022-12-29 22:11:40 -0500

    Removed export

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -1,6 +1,5 @@
 import './root';
 export { default as FrcDashboard } from './frc-dashboard';
-export { default as Dashboard } from './dashboard';
 export * from './layer';
 export {
   default as getAllowedChildren,
