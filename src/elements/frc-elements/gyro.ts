78fd7dd1948492646e69992b34ec8cfbe1a49295
amorygalili <amorygalili@gmail.com>
2022-12-31 02:52:05 -0500

    Converted gyro to typescript. Added ccw and from radian props

--- src/elements/frc-elements/gyro.ts
+++ src/elements/frc-elements/gyro.ts
@@ -1,10 +1,21 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable import/extensions */
+import { svg, css, LitElement, TemplateResult } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
 import { containerStyles } from '../styles';
-import { svg, css, LitElement } from 'lit';
 
 export const elementName = 'frc-gyro';
 
-function clamp(value, min, max) {
+function clamp(value: number, min: number, max: number) {
   return Math.min(max, Math.max(value, min));
+}
+
+function toRadians(degrees: number): number {
+  return (degrees * Math.PI) / 180;
+}
+
+function toDegrees(radians: number): number {
+  return (radians * 180) / Math.PI;
 }
 
 export const elementConfig = {
@@ -15,11 +26,25 @@ export const elementConfig = {
     value: { type: Number, primary: true },
     hideLabel: { type: Boolean, attribute: 'hide-label' },
     precision: { type: Number, defaultValue: 2 },
+    counterClockwise: { type: Boolean, attribute: 'counter-clockwise' },
+    fromRadians: { type: Boolean, attribute: 'from-radians' },
   },
 };
 
-class Gyro extends LitElement {
-  static properties = elementConfig.properties;
+@customElement('frc-gyro')
+export class Gyro extends LitElement {
+  @property({ type: Number }) value = 0;
+  @property({ type: Boolean, attribute: 'hide-label' }) hideLabel = false;
+  @property({ type: Number }) precision = 2;
+  @property({ type: Boolean, attribute: 'counter-clockwise' })
+  counterClockwise = false;
+  @property({ type: Boolean, attribute: 'from-radians' }) fromRadians = false;
+
+  tickAngles: number[] = [];
+  degreeLabelAngles: number[] = [];
+
+  @query('.container') _containerNode!: HTMLElement;
+  @query('svg') _edgeElement?: HTMLElement;
 
   static styles = [
     containerStyles,
@@ -102,12 +127,6 @@ class Gyro extends LitElement {
   constructor() {
     super();
 
-    this.value = 0;
-    this.hideLabel = false;
-    this.precision = 2;
-
-    this.tickAngles = [];
-
     for (let angle = 0; angle <= 360; angle += 5) {
       this.tickAngles.push(angle);
     }
@@ -119,35 +138,32 @@ class Gyro extends LitElement {
     }
   }
 
-  firstUpdated() {
-    this.containerNode = this.shadowRoot.querySelector('.container');
+  firstUpdated(): void {
     const resizeObserver = new ResizeObserver(() => {
       this.resized();
     });
     resizeObserver.observe(this);
   }
 
-  resized() {
+  resized(): void {
     const { width, height } = this.getBoundingClientRect();
-    this.containerNode.style.setProperty(
+    this._containerNode.style.setProperty(
       '--gyro-container-size',
       `${Math.min(width, height)}px`
     );
     this.requestUpdate();
   }
 
-  getWidth() {
-    const edgeElement = this.shadowRoot.querySelector('svg');
-
-    if (!edgeElement) {
+  getWidth(): number {
+    if (!this._edgeElement) {
       return 0;
     }
 
-    const { width } = edgeElement.getBoundingClientRect();
+    const { width } = this._edgeElement.getBoundingClientRect();
     return width;
   }
 
-  renderTicks() {
+  renderTicks(): TemplateResult {
     const width = this.getWidth();
     const innerRadius = width / 2 - width / 20;
     const outerRadius = width / 2 - width / 10;
@@ -170,7 +186,7 @@ class Gyro extends LitElement {
     `;
   }
 
-  renderEdge() {
+  renderEdge(): TemplateResult {
     const width = this.getWidth();
     const cx = width * 0.5;
     const cy = width * 0.5;
@@ -181,16 +197,20 @@ class Gyro extends LitElement {
     `;
   }
 
-  renderDialHand() {
+  renderDialHand(): TemplateResult {
     const width = this.getWidth();
 
     const outerRadius = width / 2 - width / 20 - width / 40;
 
-    const radians = ((this.value - 90) * Math.PI) / 180;
+    const radians = this.fromRadians
+      ? this.value - Math.PI / 2
+      : ((this.value - 90) * Math.PI) / 180;
+
+    const sign = this.counterClockwise ? -1 : 1;
 
     const x1 = width / 2;
     const y1 = width / 2;
-    const x2 = width / 2 + outerRadius * Math.cos(radians);
+    const x2 = width / 2 + outerRadius * Math.cos(radians) * sign;
     const y2 = width / 2 + outerRadius * Math.sin(radians);
 
     return svg`
@@ -201,7 +221,7 @@ class Gyro extends LitElement {
     `;
   }
 
-  renderDialCircle() {
+  renderDialCircle(): TemplateResult {
     const width = this.getWidth();
     const cx = width * 0.5;
     const cy = width * 0.5;
@@ -212,23 +232,23 @@ class Gyro extends LitElement {
     `;
   }
 
-  renderDegreeLabels() {
+  renderDegreeLabels(): TemplateResult {
     const width = this.getWidth();
     const radius = width / 2 + width / 100;
 
-    const edgeElement = this.shadowRoot.querySelector('svg');
-
-    if (edgeElement) {
-      edgeElement.style.setProperty(
+    if (this._edgeElement) {
+      this._edgeElement.style.setProperty(
         '--degree-label-font-size',
         `${width * 0.055}px`
       );
     }
 
+    const sign = this.counterClockwise ? -1 : 1;
+
     return svg`
       ${this.degreeLabelAngles.map((angle) => {
         const radians = ((angle - 90) * Math.PI) / 180;
-        const x = width / 2 + radius * Math.cos(radians);
+        const x = width / 2 + radius * Math.cos(radians) * sign;
         const y = width / 2 + radius * Math.sin(radians);
 
         return svg`
@@ -238,17 +258,15 @@ class Gyro extends LitElement {
     `;
   }
 
-  renderAngleLabel() {
+  renderAngleLabel(): TemplateResult | null {
     if (this.hideLabel) {
       return null;
     }
 
     const width = this.getWidth();
 
-    const edgeElement = this.shadowRoot.querySelector('svg');
-
-    if (edgeElement) {
-      edgeElement.style.setProperty(
+    if (this._edgeElement) {
+      this._edgeElement.style.setProperty(
         '--angle-label-font-size',
         `${width * 0.08}px`
       );
@@ -257,14 +275,16 @@ class Gyro extends LitElement {
     const x = width / 2;
     const y = width + width * 0.15;
 
+    const value = this.fromRadians ? toDegrees(this.value) : this.value;
+
     return svg`
       <text class="angle-label" x="${x}" y="${y}">
-        ${this.value.toFixed(clamp(this.precision, 0, 100))}&deg;
+        ${value.toFixed(clamp(this.precision, 0, 100))}&deg;
       </text>
     `;
   }
 
-  render() {
+  render(): TemplateResult {
     return svg`
       <div class="container">
         <svg>
@@ -279,5 +299,3 @@ class Gyro extends LitElement {
     `;
   }
 }
-
-customElements.define(elementName, Gyro);
