0db05d35d13ae2e09539f4ff2705d598ae5673b8
amorygalili <amorygalili@gmail.com>
2023-04-11 22:01:01 -0400

    Added swerve drivebase storybook example

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,6 @@ const config = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@ljcl/storybook-addon-cssprops',
     'storybook-addon-themes',
   ],
   framework: {
