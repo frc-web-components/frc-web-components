4a4b5a7cc90c4f1d6443b3315c858d9f0e3da858
amorygalili <amorygalili@gmail.com>
2023-04-19 22:23:44 -0400

    Added grid and flip side props to field

--- src/elements/base/field/field.ts
+++ src/elements/base/field/field.ts
@@ -23,8 +23,10 @@ export class Field extends LitElement {
   @property({ type: Number }) rotation = 0;
   @property({ type: Boolean, attribute: 'show-grid' }) showGrid = false;
   @property({ type: Number, attribute: 'grid-size' }) gridSize = 1;
+  @property({ type: Boolean, attribute: 'flip-side' }) flipSide = false;
 
-  @query('canvas', true) canvas!: HTMLCanvasElement;
+  @query('canvas', true)
+  canvas!: HTMLCanvasElement;
   @query('.container', true) container!: HTMLElement;
 
   #fieldImages: FieldImages = new FieldImages();
@@ -37,7 +39,6 @@ export class Field extends LitElement {
       display: inline-flex;
       width: 500px;
       height: 300px;
-      background: green;
       justify-content: center;
       align-items: center;
     }
@@ -61,53 +62,144 @@ export class Field extends LitElement {
     return ctx;
   }
 
+  getFieldRectPx(): { x: number; y: number; width: number; height: number } {
+    const { corners, image } = this.getConfig();
+    const { loaded, width, height } = this.#fieldImages.getImage(image);
+
+    if (!loaded) {
+      return { x: 0, y: 0, width: 0, height: 0 };
+    }
+
+    const x1 = corners.topLeft[0] / width;
+    const y1 = corners.topLeft[1] / height;
+    const x2 = corners.bottomRight[0] / width;
+    const y2 = corners.bottomRight[1] / height;
+
+    return {
+      x: x1 * this.canvas.width,
+      y: y1 * this.canvas.height,
+      width: (x2 - x1) * this.canvas.width,
+      height: (y2 - y1) * this.canvas.height,
+    };
+  }
+
+  xToPx(xUnits: number, unit: string = this.unit): number {
+    const fieldRectPx = this.getFieldRectPx();
+    const { size, unit: configUnit } = this.getConfig();
+
+    if (fieldRectPx.width === 0) {
+      return 0;
+    }
+
+    const pxPerUnit = fieldRectPx.width / size[0];
+    const xValue = convert(xUnits, unit, configUnit);
+    return !this.flipSide
+      ? fieldRectPx.x + xValue * pxPerUnit
+      : fieldRectPx.x + fieldRectPx.width - xValue * pxPerUnit;
+  }
+
+  yToPx(yUnits: number, unit: string = this.unit): number {
+    const fieldRectPx = this.getFieldRectPx();
+    const { size, unit: configUnit } = this.getConfig();
+
+    if (fieldRectPx.height === 0) {
+      return 0;
+    }
+
+    const pxPerUnit = fieldRectPx.height / size[1];
+    const yValue = convert(yUnits, unit, configUnit);
+    return !this.flipSide
+      ? fieldRectPx.y + fieldRectPx.height - yValue * pxPerUnit
+      : fieldRectPx.y + yValue * pxPerUnit;
+  }
+
   setContainerSize(): void {
     const rect = this.getBoundingClientRect();
     const src = this.getConfig().image;
     const imageObject = this.#fieldImages.getImage(src);
     const { loaded } = imageObject;
 
-    if (loaded) {
-      const boundingBoxDims = FieldImages.getBoundingBoxDims(
-        imageObject,
-        toRadians(this.rotation)
-      );
-      const dims = FieldImages.fitImageInsideBox(boundingBoxDims, {
-        width: rect.width,
-        height: rect.height,
-      });
-      const scale = dims.width / boundingBoxDims.width;
-      this.container.style.width = `${imageObject.width * scale}px`;
-      this.container.style.height = `${imageObject.height * scale}px`;
+    if (!loaded) {
+      return;
     }
+    const boundingBoxDims = FieldImages.getBoundingBoxDims(
+      imageObject,
+      toRadians(this.rotation)
+    );
+    const dims = FieldImages.fitImageInsideBox(boundingBoxDims, {
+      width: rect.width,
+      height: rect.height,
+    });
+    const scale = dims.width / boundingBoxDims.width;
+    this.container.style.width = `${imageObject.width * scale}px`;
+    this.container.style.height = `${imageObject.height * scale}px`;
   }
 
   drawImage(): void {
     const ctx = this.getCanvasCtx();
     const src = this.getConfig().image;
-    const { loaded, image, width, height } = this.#fieldImages.getImage(src);
-    console.log('draw', { loaded, image, width, height });
-    if (loaded) {
-      this.canvas.width = width;
-      this.canvas.height = height;
-      ctx.drawImage(image, 0, 0, width, height);
-      this.setContainerSize();
+    const { loaded, image } = this.#fieldImages.getImage(src);
+    if (!loaded) {
+      return;
     }
+    const { clientWidth, clientHeight } = this.canvas;
+    const canvasWidth = clientWidth * window.devicePixelRatio;
+    const canvasHeight = clientHeight * window.devicePixelRatio;
+    this.canvas.width = canvasWidth;
+    this.canvas.height = canvasHeight;
+    ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight);
+  }
+
+  drawFieldRect(): void {
+    const ctx = this.getCanvasCtx();
+    const { x, y, width, height } = this.getFieldRectPx();
+    ctx.rect(x, y, width, height);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'yellow';
+    ctx.stroke();
+  }
+
+  drawGrid(): void {
+    const ctx = this.getCanvasCtx();
+    const { image, size } = this.getConfig();
+    const { loaded } = this.#fieldImages.getImage(image);
+
+    if (!loaded || !this.showGrid || this.gridSize <= 0) {
+      return;
+    }
+
+    // Set the stroke width and color
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'gray';
+
+    for (let x = 0; x <= size[0]; x += this.gridSize) {
+      ctx.beginPath();
+      ctx.moveTo(this.xToPx(x), this.yToPx(0));
+      ctx.lineTo(this.xToPx(x), this.yToPx(size[1]));
+      ctx.stroke();
+    }
+
+    for (let y = 0; y <= size[1]; y += this.gridSize) {
+      ctx.beginPath();
+      ctx.moveTo(this.xToPx(0), this.yToPx(y));
+      ctx.lineTo(this.xToPx(size[0]), this.yToPx(y));
+      ctx.stroke();
+    }
+  }
+
+  drawField(): void {
+    this.setContainerSize();
+    this.drawImage();
+    this.drawFieldRect();
+    this.drawGrid();
+
+    window.requestAnimationFrame(() => {
+      this.drawField();
+    });
   }
 
   firstUpdated(): void {
-    this.#onImageLoadUnsub = this.#fieldImages.onImageLoad(() => {
-      this.drawImage();
-    });
-
-    const resizeObserver = new ResizeObserver(() => this.setContainerSize());
-    resizeObserver.observe(this);
-  }
-
-  updated(changedProps: Map<string, unknown>): void {
-    if (changedProps.has('game') || changedProps.has('rotation')) {
-      this.drawImage();
-    }
+    this.drawField();
   }
 
   // eslint-disable-next-line class-methods-use-this
